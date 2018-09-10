bb6309cfecba3f3d3a6684d28e0ceb6dc30b9372
VZ <coder.vz@gmail.com>
2018-09-10 07:24:15 +0700

    init starter

---
+++ views/static.paths.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ROUTES = [
+    '/',
+    '/lazy',
+    '/lazy/nested'
+];
+//# sourceMappingURL=static.paths.js.map
